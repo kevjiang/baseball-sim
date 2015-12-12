cabace8ddb811fa9e534f77724b7cc308414ed29
Kevin Jiang <k.jiang@yale.edu>
2015-12-12 17:56:09 -0500

    completed basic framework for writing explanation file for best lineup creation;

--- sim.py
+++ sim.py
@@ -32,20 +32,21 @@ def sim_and_rank_seasons(lineup, num_seasons=1000):
 
     return ordered_seasons
 
-"""
-Returns:
-1) sample top 3 best lineups
-2) player, position, runs/game table
-2b) names_to_index table
-in a tuple
 
-Note: the position is indexed 0-8, but real positions are 1-9
-Note: cannot deal with lineups with duplicate names!  Must have unique named players!
-"""
+# Returns:
+# 1) sample top 3 best lineups
+# 2) player, position, runs/game table
+# 2b) names_to_index table
+# in a tuple
 
-
+# Note: the position is indexed 0-8, but real positions are 1-9
+# Note: cannot deal with lineups with duplicate names!  Must have unique named players!
 def ordered_seasons_expl(ordered_seasons):
-    sample_best_lineups = [ordered_seasons[0], ordered_seasons[1], ordered_seasons[2]]
+    if (len(ordered_seasons) < 5):
+        print "Not enough ordered seasons: need more than 3"
+        sys.exit()
+
+    sample_best_seasons = [ordered_seasons[-1], ordered_seasons[-2], ordered_seasons[-3], ordered_seasons[-4], ordered_seasons[-5]]
     best_season = ordered_seasons[0]
     best_lineup = best_season.get_lineup()
     lineup_size = len(best_lineup)
@@ -57,7 +58,6 @@ def ordered_seasons_expl(ordered_seasons):
             sys.exit()
         name_to_index[p.get_name()] = i
 
-    print name_to_index
     #make list of lineups and associated runs per game (will be in descending run order)
     lineup_runs_list = []
     for s in ordered_seasons:
@@ -80,7 +80,39 @@ def ordered_seasons_expl(ordered_seasons):
             else:
                 runs_matrix[i][j] = col[0]/float(col[1])
 
-    return (sample_best_lineups, runs_matrix, name_to_index)
+    return (sample_best_seasons, runs_matrix, name_to_index)
+
+
+#create file with explanation of best lineup
+def explain_to_file(sample_best_seasons, runs_matrix, name_to_index):
+    fp = open('final_report.txt', 'w')
+    fp.write("Lineup Report\n\n")
+    fp.write("TLDR: pick one of these lineups for the best value: \n")
+
+    for i, season in enumerate(sample_best_seasons):
+        fp.write("Lineup %d: \n" % (i+1))
+        lineup = season.get_lineup()
+        for j, player in enumerate(lineup):
+            fp.write("%d): %s\n" % (j+1, player.get_name()))
+        fp.write("Runs per game: %f \n" % (season.get_runs_per_game()))
+        fp.write("\n")
+
+    fp.write("\n%s\n" % ("Table of Runs Per Game Based on Players by Batting Order Positions"))
+
+    # create index_to_name dictionary from name_to_index dictionary
+    index_to_name = dict((v, k) for k, v in name_to_index.iteritems())
+
+    # title the columns
+    fp.write("%25s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n" % ("Player", "Pos 1", "Pos 2", "Pos 3", "Pos 4", "Pos 5", "Pos 6", "Pos 7", "Pos 8", "Pos 9"))
+
+    # fill in the table
+    for i, row in enumerate(runs_matrix):
+        fp.write("%25s\t" % index_to_name[i])
+        for j, col in enumerate(row):
+            fp.write("%10f\t" % (runs_matrix[i][j]))
+        fp.write("\n")
+
+    fp.close()
 
 
 def main():
@@ -114,6 +146,8 @@ def main():
 
     good_tup = ordered_seasons_expl(ordered_seasons)
     print good_tup
+
+    explain_to_file(good_tup[0], good_tup[1], good_tup[2])
 
 
 if __name__ == "__main__":
